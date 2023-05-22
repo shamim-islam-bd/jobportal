f8226f94fda5313f92d64cb2c0a57d7c6e243158
shamim-islam-bd <shamimislam5091@gmail.com>
2023-05-22 16:44:15 +0600

    added

--- job/serializers.py
+++ job/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Job
 
-
+# Create your models here.
 class JobSerializers(serializers.ModelSerializer):
     class Meta:
         model = Job
